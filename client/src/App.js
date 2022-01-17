55e95f7ae491bb18df7b365f211cd209da745f1e
Sarthakjain211001 <56023046+Sarthakjain211001@users.noreply.github.com>
2022-01-17 14:12:59 +0530

    react template

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='App'>
+      <h1>Welcome to react !</h1>
+    </div>
+  )
+}
+
+export default App
